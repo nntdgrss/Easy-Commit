1207ba251f1c97b4534089da9482b040d1f150d9
nntdgrss <noweypolzovotel@gmail.com>
2025-05-20 23:23:05 +0300

    refactor: переработан код

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { $ } from "bun";
+import { commitMessages } from "./lib/commits";
+
+async function main() {
+  console.log("✨ Easy commit. Генерируем сообщение для коммита...");
+
+  const message =
+    commitMessages[Math.floor(Math.random() * commitMessages.length)];
+
+  await $`git add .`;
+  await $`git commit -m "${message}"`;
+  await $`git push`;
+
+  console.log(`💡 Создали коммит с сообщением: ${message}`);
+}
+
+main();
