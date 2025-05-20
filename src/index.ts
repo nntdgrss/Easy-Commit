db8308278d6b570ed4c817fc954508747719ac60
nntdgrss <noweypolzovotel@gmail.com>
2025-05-20 23:31:30 +0300

    fix: Исправлена критическая ошибка или баг

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,20 @@ import { commitMessages } from "./lib/commits";
 async function main() {
   console.log("✨ Easy commit. Генерируем сообщение для коммита...");
 
+  // 1. Смотрим, есть ли изменения в репозитории перед коммитом
+
+  const status = await $`git status --porcelain`.text();
+
+  if (status.trim() === "") {
+    console.log("🚫 Нет изменений в репозитории. Ничего не коммитим.");
+    return;
+  }
+
+  // 2. Генерируем сообщение для коммита
   const message =
     commitMessages[Math.floor(Math.random() * commitMessages.length)];
 
+  // 3. Добавляем все изменения в индекс
   await $`git add .`;
   await $`git commit -m "${message}"`;
   await $`git push`;
